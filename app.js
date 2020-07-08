a77c04b5ce057b6ee7d13235a12d03b7b933498b
Alex Kelber <alexkelber@Alexs-MacBook-Air.local>
2020-07-07 23:30:38 -0600

    fixed problem with app.js in production mode

--- app.js
+++ app.js
@@ -138,6 +138,14 @@ app.use("/server/complete", completeRouter);
 // app.use("/archive_authenticate", archive_authenticateRouter);
 // app.use("/archive_game", archive_gameRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
